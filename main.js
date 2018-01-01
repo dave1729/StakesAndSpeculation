3144260d889793e62aac8638b7ed24977121ca37
dave1729 <Dave1729@uw.edu>
2018-01-01 00:17:40 -0800

    Displays list of Game Ids as well as Riddle Data. Also has new game and join game buttons to use in the future to decide if the client should act as a the gameboard or as a user.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 function SecretSantaInterface() {
 
 	//alert("function SecretSantaInterface()");
@@ -20,13 +21,79 @@ SecretSantaInterface.prototype.drawAll = function() {
 	window.requestAnimationFrame(this.drawAll.bind(this));
 }
 
-function getQuestions()
-{
-    var response = httpGet("https://sheets.googleapis.com/v4/spreadsheets/1GSNfhGn2CeTzpqgQik0LpXdivIy4NDQYye06o3m1eD4/values/Questions?key=AIzaSyAA_GuyTMBfuNwfVwFFahFiVZql23-xe-Y&majorDimension=ROWS");
+function StartNewGame() {
+    document.getElementById('button1').style.visibility = 'hidden';
+    document.getElementById('button2').style.visibility = 'hidden';
+    document.getElementById('button4').style.visibility = 'hidden';
+    //role = "server";
+    currentGameRiddles = getRiddlesFromServer();
+    httpPost();
+}
 
-    var riddles = riddleListFromResponse(response);
+function JoinGame() {
+    document.getElementById('button1').style.visibility = 'hidden';
+    document.getElementById('button2').style.visibility = 'hidden';
+    document.getElementById('button3').style.visibility = 'hidden';
+    //role = "client";
+}
+
+function displayCurrentGame() {
+    var games = getGamesFromServer();
+
+    var retVal = "";
+
+    for(var i = 0; i < games.length; i++) {
+        var game = games[i];
+        retVal +=
+            game.Id + ": " + "____" +
+            game.gameId + "<br>";
+    }
+
+    return retVal;
+}
+
+function displayRiddles()
+{
+    var riddles = getRiddlesFromServer();
 
     return riddlesAsString(riddles);
+}
+
+function getGamesFromServer()
+{
+    var response = httpGet("https://sheets.googleapis.com/v4/spreadsheets/1GSNfhGn2CeTzpqgQik0LpXdivIy4NDQYye06o3m1eD4/values/Games?key=AIzaSyAA_GuyTMBfuNwfVwFFahFiVZql23-xe-Y&majorDimension=ROWS");
+
+    return gameListFromResponse(response);
+}
+
+function postGameToServer()
+{
+    var response = httpGet("https://sheets.googleapis.com/v4/spreadsheets/1GSNfhGn2CeTzpqgQik0LpXdivIy4NDQYye06o3m1eD4/values/Games?key=AIzaSyAA_GuyTMBfuNwfVwFFahFiVZql23-xe-Y&majorDimension=ROWS");
+
+    return gameListFromResponse(response);
+}
+
+function gameListFromResponse(httpResponse)
+{
+    var arrayOfArrays = JSON.parse(httpResponse);
+    var games = [];
+
+    for(var i = 0; i < arrayOfArrays.values.length; i++) {
+        var game = new Object();
+        var array = arrayOfArrays.values[i];
+        game.id = i + 1;
+        game.gameId = array[0];
+        games.push(game);
+    }
+
+	return games;
+}
+
+function getRiddlesFromServer()
+{
+    var response = httpGet("https://sheets.googleapis.com/v4/spreadsheets/1GSNfhGn2CeTzpqgQik0LpXdivIy4NDQYye06o3m1eD4/values/Riddles?key=AIzaSyAA_GuyTMBfuNwfVwFFahFiVZql23-xe-Y&majorDimension=ROWS");
+
+    return riddleListFromResponse(response);
 }
 
 function riddlesAsString(riddles) {
@@ -52,6 +119,7 @@ function riddleListFromResponse(httpResponse)
     for(var i = 0; i < arrayOfArrays.values.length; i++) {
         var riddle = new Object();
         var array = arrayOfArrays.values[i];
+        riddle.id = i + 1;
         riddle.question = array[0];
         riddle.answer = array[1];
         riddle.units = array[2];
@@ -63,16 +131,11 @@ function riddleListFromResponse(httpResponse)
         riddle.creationdate = array[8];
         riddle.createdby = array[9];
         riddle.lastuseddate = array[10];
-        console.log(array[0] + " - " + array[1] + " - " + array[2] + " - " + array[3]);
         riddles.push(riddle);
     }
 
     console.log("Retrieved Riddles:");
-
-    for(var i = 0; i < riddles.length; i++) {
-        var riddle = riddles[i];
-        console.log("Riddle " + i + ": " + riddle.question);
-    }
+    console.log(riddlesAsString(riddles));
 
 	return riddles;
 }
@@ -83,6 +146,37 @@ function httpGet(theUrl)
     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
     xmlHttp.send( null );
     return xmlHttp.responseText;
+}
+
+//this function doesn't work
+function httpPost() {
+    var http = new XMLHttpRequest();
+    var url = "https://sheets.googleapis.com/v4/spreadsheets/1GSNfhGn2CeTzpqgQik0LpXdivIy4NDQYye06o3m1eD4/values/Games!A1:append?key=AIzaSyAA_GuyTMBfuNwfVwFFahFiVZql23-xe-Y&majorDimension=ROWS?Game=9999";
+    var body = '{' +
+                  '"range": "Games",' +
+                  '"majorDimension": "ROWS",' +
+                  '"values": [[' +
+                  '"1",' +
+                  '"2",' +
+                  '"3",' +
+                  '"4"' +
+                  ']],' +
+                '}';
+
+    http.open("POST", url, true);
+
+    //Send the proper header information along with the request
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    http.setRequestHeader("Access-Control-Allow-Origin", "*");
+
+    http.onreadystatechange = function() {//Call a function when the state changes.
+        if(http.readyState == 4 && http.status == 200) {
+            alert(http.responseText);
+        }
+    }
+    http.send(body);
+    console.log("response " + http.response);
+    console.log("responsetext " + http.responseText);
 }
 
 var ssInterface = new SecretSantaInterface();
