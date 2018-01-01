7ec2b2549b9de873f72de43781e518186d7639b2
dave1729 <Dave1729@uw.edu>
2017-12-31 20:55:10 -0800

    Displays questions only. All Riddles(object containing the question, answer, unit, source, etc.) are retrieved from the google doc.

--- main.js
+++ main.js
@@ -22,40 +22,59 @@ SecretSantaInterface.prototype.drawAll = function() {
 
 function getQuestions()
 {
-//    var responseText = httpGet("https://docs.google.com/spreadsheets/d/1GSNfhGn2CeTzpqgQik0LpXdivIy4NDQYye06o3m1eD4/edit?usp=sharing");
-//
-//    var myRe = /Question/;
-//    var start = responseText.search(myRe);
-//    var myRe = /endoflistterminator/;
-//    var end = responseText.search(myRe);
-//    var retVal;
-//
-//    if(start !== -1) {
-//        console.log("Success " + responseText.substring(start, end));
-//
-//        retVal = responseText.substring(start, end);
-//    }
-//    else {
-//        console.log("Fail " + responseText);
-//        retVal = responseText;
-//    }
+    var response = httpGet("https://sheets.googleapis.com/v4/spreadsheets/1GSNfhGn2CeTzpqgQik0LpXdivIy4NDQYye06o3m1eD4/values/Questions?key=AIzaSyAA_GuyTMBfuNwfVwFFahFiVZql23-xe-Y&majorDimension=ROWS");
 
-    var response = httpGet("https://sheets.googleapis.com/v4/spreadsheets/1GSNfhGn2CeTzpqgQik0LpXdivIy4NDQYye06o3m1eD4/values/Questions?key=AIzaSyAA_GuyTMBfuNwfVwFFahFiVZql23-xe-Y");
-    var myRe = /Question/;
-    var start = response.search(myRe);
-    var myRe = /endoflistterminator/;
-    var end = response.search(myRe);
+    var riddles = riddleListFromResponse(response);
 
-    if(start !== -1) {
-        console.log("Success " + response.substring(start, end));
-        retVal = response.substring(start, end);
+    return riddlesAsString(riddles);
+}
+
+function riddlesAsString(riddles) {
+    var retVal = "";
+
+    for(var i = 0; i < riddles.length; i++) {
+        var riddle = riddles[i];
+        retVal +=
+            "Riddle " + i + ": " + "____" +
+            riddle.question + "____" +
+            riddle.answer + "    " +
+            riddle.units + "<br>";
     }
-    else {
-        console.log("Fail " + response);
-        retVal = response;
+
+    return retVal;
+}
+
+function riddleListFromResponse(httpResponse)
+{
+    var arrayOfArrays = JSON.parse(httpResponse);
+    var riddles = [];
+
+    for(var i = 0; i < arrayOfArrays.values.length; i++) {
+        var riddle = new Object();
+        var array = arrayOfArrays.values[i];
+        riddle.question = array[0];
+        riddle.answer = array[1];
+        riddle.units = array[2];
+        riddle.funfact = array[3];
+        riddle.sourcename = array[4];
+        riddle.source = array[5];
+        riddle.sourcetype = array[6];
+        riddle.sourceyear = array[7];
+        riddle.creationdate = array[8];
+        riddle.createdby = array[9];
+        riddle.lastuseddate = array[10];
+        console.log(array[0] + " - " + array[1] + " - " + array[2] + " - " + array[3]);
+        riddles.push(riddle);
     }
-	////var number = window.prompt("Number?");
-	return retVal;
+
+    console.log("Retrieved Riddles:");
+
+    for(var i = 0; i < riddles.length; i++) {
+        var riddle = riddles[i];
+        console.log("Riddle " + i + ": " + riddle.question);
+    }
+
+	return riddles;
 }
 
 function httpGet(theUrl)
