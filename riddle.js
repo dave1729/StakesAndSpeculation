d3dbf4ee9c463d0a489a82dfb67a493aea3281d5
dave1729 <Dave1729@uw.edu>
2018-02-04 20:56:15 -0800

    Added ability to add participants to the current game.

--- riddle.js
+++ riddle.js
@@ -1,72 +1,18 @@
-var riddles = [];
-
-function StartNewGame() {
-    //document.getElementById('button1').style.visibility = 'hidden';
-    //document.getElementById('button2').style.visibility = 'hidden';
-    //document.getElementById('button3').style.visibility = 'hidden';
-    //document.getElementById('button4').style.visibility = 'hidden';
-
-    //TODO display game board
-
-    //TODO display question
-
-    //TODO poll for changes
-
-    return riddles[0].answer;
+function Riddle(riddleAsArray) {
+   this.id = riddleAsArray[0];
+   this.question = riddleAsArray[1];
+   this.answer = riddleAsArray[2];
+   this.units = riddleAsArray[3];
+   this.funfact = riddleAsArray[4];
+   this.sourcename = riddleAsArray[5];
+   this.source = riddleAsArray[6];
+   this.sourcetype = riddleAsArray[7];
+   this.sourceyear = riddleAsArray[8];
+   this.creationdate = riddleAsArray[9];
+   this.createdby = riddleAsArray[10];
+   this.lastuseddate = riddleAsArray[11];
 }
 
-function JoinGame() {
-    //document.getElementById('button1').style.visibility = 'hidden';
-    //document.getElementById('button2').style.visibility = 'hidden';
-    //document.getElementById('button3').style.visibility = 'hidden';
-    //document.getElementById('button4').style.visibility = 'hidden';
-
-    var serverJsonAsText = GetJson();
-
-    riddles = JSON.parse(serverJsonAsText);
-
-    //TODO display current question
-
-    //TODO display testbox for answer
-
-    //TODO send answer
-
-    return riddles[0].question;
-}
-
-function GetJson() {
-    var xmlHttp = new XMLHttpRequest();
-    var theUrl = "https://api.myjson.com/bins/10zvwh";
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.onreadystatechange = function() {//Call a function when the state changes.
-        if(xmlHttp.readyState == XMLHttpRequest.DONE && xmlHttp.status == 200) {
-            console.log("Post returned 200! " + xmlHttp.responseText);
-            riddles = JSON.parse(xmlHttp.responseText);
-        }
-        else {
-        console.log("XML ReadyState: " + xmlHttp.readyState + " Status: " + xmlHttp.status);
-        }
-    }
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
-
-function PutJson() {
-    var xmlHttp = new XMLHttpRequest();
-    var theUrl = "https://api.myjson.com/bins/10zvwh";
-    xmlHttp.open( "PUT", theUrl, false ); // false for synchronous request
-    xmlHttp.setRequestHeader("Content-Type", "application/json");
-    xmlHttp.onreadystatechange = function() {//Call a function when the state changes.
-        if(xmlHttp.readyState == XMLHttpRequest.DONE && xmlHttp.status == 200) {
-            console.log("Put Success " + xmlHttp.responseText);
-        }
-        else {
-        console.log("XML ReadyState: " + xmlHttp.readyState + " Status: " + xmlHttp.status);
-        }
-    }
-
-    var json = JSON.stringify(riddles);
-
-    xmlHttp.send( json );
-    return xmlHttp.responseText;
+Riddle.prototype.addGroup = function(newGroupName, ctx) {
+	this.currentgroup = new InputGroup(newGroupName, ctx);
 }
